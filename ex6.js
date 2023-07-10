917c8415056f2a3798411883b291ee4ed92115ab
camilabsp <120345334+camilabsp@users.noreply.github.com>
2023-07-10 16:49:26 -0300

    ex6
    Escreva uma função que lê o valor da variável x. Calcule e mostre o resultado da expressão: 9x - 4x + 10.

---
+++ ex6.js
@@ -0,0 +1,9 @@
+//Escreva uma função que lê o valor da variável x. Calcule e mostre o resultado da expressão: 9x - 4x + 10.
+
+function expressao (x) {
+    return 9 * x - 4 * x + 10  
+}
+
+console.log(expressao(5));
+console.log(expressao(0));
+console.log(expressao(-2));

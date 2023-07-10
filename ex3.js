d75a4a512c4f2ff57aa62c882ba7bae72e7e2d72
camilabsp <120345334+camilabsp@users.noreply.github.com>
2023-07-10 16:47:46 -0300

    ex3
    // Escreva um programa que mostre na tela as mensagens:
    //Alô, mundo!
    //ALO MUNDO
    //alô, mundo.
    //ALo munDO
    //Alô, Mundo!

---
+++ ex3.js
@@ -0,0 +1,12 @@
+// Escreva um programa que mostre na tela as mensagens:
+//Alô, mundo!
+//ALO MUNDO
+//alô, mundo.
+//ALo munDO
+//Alô, Mundo!
+
+console.log('Alô, mundo!');
+console.log('ALO MUNDO');
+console.log('alô, mundo.');
+console.log('ALo munDO');
+console.log('Alô, Mundo!');

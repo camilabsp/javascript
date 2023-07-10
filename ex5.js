477e234afe00b974501de1ed701c6b3be58839ee
camilabsp <120345334+camilabsp@users.noreply.github.com>
2023-07-10 16:48:56 -0300

    ex5
    Escreva uma função que leia separadamente nome e sobrenome, e mostre na tela o nome completo

---
+++ ex5.js
@@ -0,0 +1,7 @@
+//Escreva uma função que leia separadamente nome e sobrenome, e mostre na tela o nome completo
+
+function nomeCompleto (nome, sobrenome) {
+    return nome + ' ' + sobrenome;
+}
+
+console.log(nomeCompleto('Camila','Lima'));
